437ca7d9656d2708ac2cb308f97b0f7963952ea8
rpizarrog <rpizarro@itdurango.edu.mx>
2022-03-11 17:56:32 -0600

    Add files via upload

---
+++ scripts/construir data frame a partir de vectores.R
@@ -0,0 +1,8 @@
+# Construr un conjunto de datos
+# a partir de vectores
+edades <- c(23, 24, 54, 56, 54, 57, 65, 34)
+nombres <- c("RUBEN", "ANGEL I", "JOSUE", 
+             "CARLOS",'PAty', 'LULU', 'OSCAR', 'LALO')
+
+datos <- data.frame(nombres, edades)
+datos
