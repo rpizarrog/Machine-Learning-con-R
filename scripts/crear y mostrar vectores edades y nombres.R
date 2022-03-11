29ee06adeb4d32dbf0eb6b1ff67a97f0306e32d8
rpizarrog <rpizarro@itdurango.edu.mx>
2022-03-11 17:48:52 -0600

    Add files via upload

---
+++ scripts/crear y mostrar vectores edades y nombres.R
@@ -0,0 +1,9 @@
+# Uso de vectores y data.frames
+# Vectors
+edades <- c(23, 24, 54, 56, 54, 57, 65, 34)
+nombres <- c("RUBEN", "ANGEL I", "JOSUE", 
+             "CARLOS",'PAty', 'LULU', 'OSCAR', 'LALO')
+
+edades
+nombres
+
