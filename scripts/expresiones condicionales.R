cbb6fd197db7a8d0b10bcfb1839ddc13e2a0cdd3
rpizarrog <rpizarro@itdurango.edu.mx>
2022-03-11 19:33:45 -0600

    Add files via upload

---
+++ scripts/expresiones condicionales.R
@@ -0,0 +1,78 @@
+x <- 20
+y <- 30
+
+# Expresiones
+
+# Condicionales
+# < > <= >= != 
+if (x <= y) { 
+  print ("x es menor que y")
+} else {
+  print ("x es mayor que y")
+} 
+
+# Logico
+# | or
+# & and
+# ! not
+if (x < y | x == y) { 
+  print ("x es menor o igual que y")
+} else {
+  print ("x es mayor que y")
+}
+
+nombre <- "ruben"
+if (is.character(nombre))
+  print ("El nombre es tipo char")
+
+numero <- "5"
+if (is.numeric(numero)) {
+  print ("Si es un numero")
+} else {
+  print ("No es numérico")
+} 
+
+x = 5
+
+if (x %% 2) {
+  print ("Es numero par")
+} else if (x < 3) {
+  print ("x de menor que 3")
+} else {
+  paste ("x es ", x)
+}
+
+# Convertir un valor numerico a texto
+x = 5
+# cuatro
+if (x < 10) {
+  if (x == 1){
+    texto = "uno"
+  }
+  else if (x == 2){
+    texto = "dos"
+  }
+  else if (x == 3){
+    texto = "tres"
+  }
+  else if (x == 4){
+    texto = "cuatro"
+  }
+  else if (x == 5){
+    texto = "cinco"
+  }
+  texto
+
+}
+
+  
+
+
+
+
+
+
+
+
+
+
