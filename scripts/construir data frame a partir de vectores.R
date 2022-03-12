21beab4ee0b131a94f2447ce428db1ef53a2d5ce
rpizarrog <rpizarro@itdurango.edu.mx>
2022-03-11 19:58:15 -0600

    Add files via upload

--- scripts/construir data frame a partir de vectores.R
+++ scripts/construir data frame a partir de vectores.R
@@ -3,6 +3,8 @@
 edades <- c(23, 24, 54, 56, 54, 57, 65, 34)
 nombres <- c("RUBEN", "ANGEL I", "JOSUE", 
              "CARLOS",'PAty', 'LULU', 'OSCAR', 'LALO')
+automoviles <- c("VW", "Chrysl", "Ford")
+
 
 datos <- data.frame(nombres, edades)
 datos
---
+++ scripts/mandar llamar funciones.R
@@ -0,0 +1,9 @@
+# MAndar llamr mis funciones
+
+# source("funciones/mis funciones.r")
+
+source("https://raw.githubusercontent.com/rpizarrog/Machine-Learning-con-R/main/funciones/mis%20funciones.R")
+
+f.saludos("Ruben")
+
+f.convertir.dt(7)
